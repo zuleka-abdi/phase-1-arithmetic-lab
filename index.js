46d6318769479a50294da26ad8235a0c7d42ac31
zuleka abdirashid <zulexzulex90@gmail.com>
2024-12-08 10:58:24 +0300

    adds

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const num1 = 31; 
+const num2 = 2;  
+const multiply = num1 * num2;
 
-// Write your code here
+
+const random = Math.floor(Math.random() * 100) + 1;
+
+
+const num3 = 28;
+const num4 = 6;  
+const mod = num3 % num4; 
+
+
+const max = Math.max(1, 5, 10, 20); 
+
+
